73144f4ca03c7e22c438e4b031a73517ac568485
Mike Hamer <mhamer@bandwidth.com>
2020-11-03 14:09:31 -0500

    Add Bandwidth.Sdk package and remove default project items

---
+++ Server/Controllers/CallbacksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CallbacksController : ControllerBase
+    {
+        private readonly ILogger<CallbacksController> _logger;
+
+        public CallbacksController(ILogger<CallbacksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("callInitiatedCallback")]
+        public ActionResult CallInitiatedCallback()
+        {
+            _logger.LogInformation($"{Request.Path.Value} requested.");
+
+            return new OkResult();
+        }
+    }
+}
