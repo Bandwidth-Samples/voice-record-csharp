521269bedc88be57891f77cd1f57aa729a024f87
Mike Hamer <mhamer@bandwidth.com>
2020-11-03 16:11:22 -0500

    Add callbacks to allow recording

--- Server/Controllers/CallbacksController.cs
+++ Server/Controllers/CallbacksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bandwidth.Standard.Voice.Bxml;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Server.Controllers
@@ -16,6 +17,58 @@ namespace Server.Controllers
 
         [HttpPost("callInitiatedCallback")]
         public ActionResult CallInitiatedCallback()
+        {
+            _logger.LogInformation($"{Request.Path.Value} requested.");
+
+            var unavailableSpeakSentence = new SpeakSentence
+            {
+                Sentence = "Vandelay Industries, Kal Varnsen is unavailable at this time."
+            };
+
+            var messageSpeakSentence = new SpeakSentence
+            {
+                Sentence = "At the tone, please record your message, when you have finished recording, you may hang up."
+            };
+
+            var playAudio = new PlayAudio
+            {
+                Url = "/files/tone"
+            };
+
+            var record = new Record
+            {
+                RecordCompleteUrl = "/callbacks/recordCompleteCallback",
+                RecordingAvailableUrl = "/callbacks/recordingAvailableCallback"
+            };
+
+            var response = new Response(
+                unavailableSpeakSentence,
+                messageSpeakSentence,
+                playAudio,
+                record
+            );
+
+            return new OkObjectResult(response.ToBXML());
+        }
+
+        [HttpPost("callAnsweredCallback")]
+        public ActionResult CallAnsweredCallback()
+        {
+            _logger.LogInformation($"{Request.Path.Value} requested.");
+
+            return new OkResult();
+        }
+
+        [HttpPost("recordCompleteCallback")]
+        public ActionResult RecordCompleteCallback()
+        {
+            _logger.LogInformation($"{Request.Path.Value} requested.");
+
+            return new OkResult();
+        }
+
+        [HttpPost("recordingAvailableCallback")]
+        public ActionResult RecordingAvailableCallback()
         {
             _logger.LogInformation($"{Request.Path.Value} requested.");
 
